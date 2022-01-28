be6049d80f170bc2fb512439ab1aaf39041d3429
binoy638 <binoy638@gmail.com>
2022-01-28 13:00:07 +0530

    added express middlewares

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import express, { Request, Response } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import helmet from 'helmet';
 
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+
+//* Middilewares
+app.use(helmet());
+app.use(morgan('tiny'));
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send({
